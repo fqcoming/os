518a61bf8b2203dd0f786073971231688a30c2c6
fuqiang <fuqiang@gmail.com>
2023-06-29 00:13:53 +0800

    ok

--- x86os/src/source/kernel/include/core/memory.h
+++ x86os/src/source/kernel/include/core/memory.h
@@ -6,6 +6,12 @@
 #include "comm/boot_info.h"
 #include "ipc/mutex.h"
 
+
+
+
+
+
+
 #define MEM_EBDA_START              0x00080000
 #define MEM_EXT_START               (1024*1024)
 #define MEM_EXT_END                 (128*1024*1024 - 1)
@@ -38,16 +44,22 @@ typedef struct _memory_map_t {
     uint32_t perm;      // 访问权限
 }memory_map_t;
 
-void memory_init (boot_info_t * boot_info);
-uint32_t memory_create_uvm (void);
+
+
+
+void     memory_init               (boot_info_t * boot_info);
+uint32_t memory_create_uvm         (void);
 uint32_t memory_alloc_for_page_dir (uint32_t page_dir, uint32_t vaddr, uint32_t size, int perm);
-int memory_alloc_page_for (uint32_t addr, uint32_t size, int perm);
-uint32_t memory_alloc_page (void);
-void memory_free_page (uint32_t addr);
-void memory_destroy_uvm (uint32_t page_dir);
-uint32_t memory_copy_uvm (uint32_t page_dir);
-uint32_t memory_get_paddr (uint32_t page_dir, uint32_t vaddr);
-int memory_copy_uvm_data(uint32_t to, uint32_t page_dir, uint32_t from, uint32_t size);
+int      memory_alloc_page_for     (uint32_t addr, uint32_t size, int perm);
+uint32_t memory_alloc_page         (void);
+void     memory_free_page          (uint32_t addr);
+void     memory_destroy_uvm        (uint32_t page_dir);
+uint32_t memory_copy_uvm           (uint32_t page_dir);
+uint32_t memory_get_paddr          (uint32_t page_dir, uint32_t vaddr);
+int      memory_copy_uvm_data      (uint32_t to, uint32_t page_dir, uint32_t from, uint32_t size);
+
 char * sys_sbrk(int incr);
+
+
 
 #endif // MEMORY_H
